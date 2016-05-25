04971d06aa1947ed704249cb6bed5d0d748a814f
Krista <kristina.komanova@gmail.com>
2016-05-25 18:32:03 +0200

    solution with magic

--- Bridge.java
+++ Bridge.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//package bridges;
+//package bridge;
 
 import java.util.Comparator;
 import java.util.Scanner;
@@ -15,141 +15,115 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 
-class Bridges {
-static int n,m,k;
-static Vertex[] students;
-static ArrayList<int[]> allEdges;
-static ArrayList<Vertex> list;
-static int id=0;
+class Bridge {
 
-static class Vertex{
-     ArrayList<Vertex> neighbours; 
-     boolean know;
-     int color;
-     int ID;
-     
-     Vertex(){
-         this.neighbours = new ArrayList();
-         this.know = false;
-         this.color = 0;
-         ID=id++;
-     }         
- }
- 
- static boolean findCritical(Vertex a, Vertex b){  
-     boolean exist;
-       a.neighbours.remove(b);
-       b.neighbours.remove(a);
-       a.color = 1; b.color = 2;
-       exist = lookAt(a);
-       if (exist){
-           a.color = 2; b.color = 3;
-           list.clear();
-           /* for(Vertex v: students) {
-              if(v.color != 2) v.color = 0;
-           }//reset
-           exist=lookAt(b);*/
-           exist=lookAt3(b);
-       }
-       a.neighbours.add(b);
-       b.neighbours.add(a);
-    return !exist;
- }
- 
- static boolean lookAt(Vertex v){ //find someone who know the answer
-     if (v.know == true) return true;
-     list.add(v);
+    static int n, m, k;
+    static Vertex[] students;
+    static ArrayList<int[]> allEdges;
+    static Vertex[] virtualVertex;
+    static ArrayList<Vertex> list;
+    static int id = 0;
+    static boolean[] visited;
+    static int t = 0;
+    static PriorityQueue<int[]> Edges ;
 
-     while (!list.isEmpty()) {
-         Vertex u = list.remove(0);
-         for (Vertex x : u.neighbours) { //only color = 0
-             if (x.color == 0) {
-                 if (x.know)return true;
-                 list.add(x);
-                 x.color = 1;
-             }
-             if (x.color == 2) {
-                 return true;
-             }
-         }
-     }
-     return false;
- }
- 
- static boolean lookAt3(Vertex v){ //find someone who know the answer
-     if (v.know == true) return true;
-     list.add(v);
+    static class Vertex {
 
-     while (!list.isEmpty()) {
-         Vertex u = list.remove(0);
-         for (Vertex x : u.neighbours) { //only color = 0
-             if (x.color == 0) {
-                 if (x.know)return true;
-                 list.add(x);
-                 x.color = 3;
-             }
-             else if(x.color < 3) {
-                 return true;
-             }
-         }
-     }
-     return false;
- }
- 
-     
+        ArrayList<Vertex> neighbours;
+        boolean know;
+        int time;
+        int ID;
+        int LL;
+        boolean visited;
+        boolean bridge;
+        int num;
+
+        Vertex() {
+            this.neighbours = new ArrayList();
+            this.know = false;
+            this.time = -1;
+            ID = id++;
+        }
+    }
+
+    static void dfs(Vertex v, Vertex parent) {
+        v.visited = true;
+        v.time = t++;
+        v.LL = v.time;
+        if(v.know)v.num++;
+        //int num_child = 0;
+        for (int i = 0; i < v.neighbours.size(); i++) {
+            Vertex w = v.neighbours.get(i);
+            if (w == parent) {continue;}
+            if (w.visited) {
+                v.LL = Math.min(v.LL, w.time);
+                continue;
+            }
+        //    num_child++;
+            dfs(w, v);
+            v.num += w.num;
+            v.LL = Math.min(v.LL, w.LL);
+            //System.out.println(v.ID + " " + w.ID  + " :" + w.num + v.num);
+            if (w.LL >= v.time && (w.num == 0 || w.num == k)) {
+                v.bridge = true;}
+        }
+        
+    }
+
     public static void main(String[] args) {
         list = new ArrayList<>();
         Scanner s = new Scanner(System.in);
         n = s.nextInt(); //num students
         m = s.nextInt(); // num canals
         k = s.nextInt(); // num of knowers
-        
-        students = new Vertex[n+1];
-        for( int i = 0; i< n+1; i++) students[i]= new Vertex();
-        for (int i = 0; i< k; i++){
+
+        students = new Vertex[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            students[i] = new Vertex();
+        }
+        for (int i = 0; i < k; i++) {
             students[s.nextInt()].know = true;
         }
-        
-        int a,b;
+
+        int a, b;
         allEdges = new ArrayList<>();
-        
-        
-        for (int i = 0; i < m; i++){
+        virtualVertex = new Vertex[m];
+
+        for (int i = 0; i < m; i++) {
             a = s.nextInt();
             b = s.nextInt();
-            students[a].neighbours.add(students[b]);
-            students[b].neighbours.add(students[a]);
-            int[] z= new int[2];
-            if (a < b) {
-                z[0]= a; z[1]=b;
-            } else {
-                z[0]= b; z[1]=a;
-            }
-            allEdges.add(z);
+            virtualVertex[i] = new Vertex();
+            virtualVertex[i].neighbours.add(students[a]);
+            virtualVertex[i].neighbours.add(students[b]);
+            students[a].neighbours.add(virtualVertex[i]);
+            students[b].neighbours.add(virtualVertex[i]);
+          
         }
+
+        Edges = new PriorityQueue<>(m, new Comparator<int[]>() {
+            public int compare(int[] student1, int[] student2) {
+                return (student1[0] == student2[0]) ? (Integer.valueOf(student1[1]).compareTo(student2[1]))
+                        : (student1[0] - student2[0]);
+            }
+        });
+
         
+        dfs(students[1], null);
        
-        PriorityQueue<int[]> Edges = new PriorityQueue<>(m, new Comparator<int[]>() {
-        public int compare(int[] student1, int[] student2) {
-            return (student1[0] == student2[0]) ? (Integer.valueOf(student1[1]).compareTo(student2[1]))
-                 : (student1[0]- student2[0]);
-        }
-    });
-        
-        for(int[] i: allEdges){
-            if (findCritical(students[i[0]], students[i[1]])){
-                Edges.add(i);
+        for(Vertex v: virtualVertex){
+            if (v.bridge){
+                 int[] e = new int[2];
+                e[0]=Math.min(v.neighbours.get(0).ID, v.neighbours.get(1).ID);
+                e[1]=Math.max(v.neighbours.get(0).ID, v.neighbours.get(1).ID);
+                Edges.add(e);
             }
-            list.clear();
-            for(Vertex v: students) v.color = 0;//reset
         }
-        
-        int[] e = new int[2];
+         int[] e = new int[2];
         System.out.println(Edges.size());
-        while(!Edges.isEmpty()){
-            e= Edges.poll();
-            System.out.println(e[0]+" "+e[1]);
+        while (!Edges.isEmpty()) {
+            e = Edges.poll();
+            System.out.println(e[0] + " " + e[1]);
         }
     }
-    
+
 }
